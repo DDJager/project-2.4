a398ebe80ed67c998445082211554b1db78c4a0e
KingInktvis <rikdevries253@hotmail.nl>
2017-05-31 15:53:01 +0200

    Added React.

---
+++ front-end/src/footer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rik on 5/25/17.
+ */
+import React from 'react';
+import './style/footer.css';
+
+const Footer = function () {
+    return (
+        <div className="footer">
+            this is at the bottom
+        </div>
+    );
+};
+
+export default Footer;
---
+++ front-end/src/header.js
@@ -0,0 +1,21 @@
+/**
+ * Created by rik on 5/25/17.
+ */
+import React, {Component} from 'react';
+import './style/header.css';
+
+class Header extends Component {
+
+
+    render() {
+        return (
+            <div className="header">
+                <nav>
+
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Header;
