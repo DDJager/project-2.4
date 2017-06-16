2ac9b54b8540254701a427c9f31e624a9915cd60
KingInktvis <rikdevries253@hotmail.nl>
2017-06-16 16:27:16 +0200

    Selected game is now controlled by state.

--- website/main/front-end/src/components/achievements.js
+++ website/main/front-end/src/components/achievements.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import _ from 'lodash';
 
 import Achievement from './achievement';
 
-const Achievements = (props)=> {
-   return (
-       <div>
-           <h3>Achievements</h3>
-           <Achievement name="ak" image="favicon.ico"/>
-           {/*list of all owned achievements*/}
-       </div>
-   )
-};
+class Achievements extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedGame: ''
+        }
+        this.onChangeHandle = this.onChangeHandle.bind(this);
+    }
+
+    dropdownOptions() {
+        return (
+            _.map(this.props.games, this.option)
+        );
+    }
+
+    option(game) {
+        return (
+            <option key={game.id} value={game.id}>{game.name}</option>
+        )
+    }
+
+    onChangeHandle(game) {
+        this.setState({
+            selectedGame: game.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+
+                <h3>Achievements</h3>
+                <select
+                    onChange={this.onChangeHandle}
+                    value={this.state.selectedGame}
+                >
+                    {this.dropdownOptions()}
+                </select>
+                <Achievement name="ak" image="favicon.ico"/>
+                {/*list of all owned achievements*/}
+            </div>
+        );
+    }
+}
 
 export default Achievements;
--- website/main/front-end/src/reducers/games_reducer.js
+++ website/main/front-end/src/reducers/games_reducer.js
@@ -2,8 +2,7 @@ import { GAMES } from '../actions/index';
 
 export default function games(state = {}, action) {
     if (action.type === GAMES) {
-        state = action.payload;
-        console.log(state);
+        state = action.payload.data;
     }
     return state;
 }
