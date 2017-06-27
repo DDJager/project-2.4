af53f7501aedfc940a22dd46672b902017f5e65d
jlsa <jlsahoekstra@gmail.com>
2017-06-27 14:21:36 +0200

    clean up achievements route code

--- website/main/api_1_0/achievements.py
+++ website/main/api_1_0/achievements.py
@@ -34,32 +34,7 @@ def get_achievements_from_user(user_id):
 @auth.login_required
 def get_achievements_where_user_id_is(user_id):
     achievements = get_achievements_from_user(user_id)
-    # # select * from achievements_users
-    # achievements = Achievement.query.all()
-    # achievements_users = AchievementUser.query.filter_by(user_id=user_id).all()
-    #
-    # # Create an empty list
-    # data = [ ]
-    #
-    # # Loop through all achievements
-    # for achievement_user in achievements_users:
-    #     achievement_id = achievement_user.achievement_id
-    #     achievement = Achievement.query.filter_by(id=achievement_id).first()
-    #
-    #     game = Game.query.filter_by(id=achievement.game_id).first()
-    #     user = User.query.filter_by(id=user_id).first()
-    #
-    #
-    #     data.append({
-    #         'name': achievement.name,
-    #         'description': achievement.description,
-    #         'game': {
-    #             'id': game.id,
-    #             'name': game.name,
-    #             'description': game.description
-    #         }
-    #     })
-
+    
     return jsonify({
         'status': 'success',
         'achievements': achievements
@@ -77,31 +52,3 @@ def get_achievements():
         'status': 'success',
         'achievements': achievements
     }), 200
-    # # SELECT * FROM users
-    # achievements = Achievement.query.all()
-    #
-    # # Create an empty list
-    # data = [
-    #
-    # ]
-    #
-    # # Loop trough all users. Append a list with the db row data everytime
-    # for achievement in achievements:
-    #     # For every achievement, grab a game object, by their id
-    #     game = Game.query.filter_by(id=achievement.game_id).first()
-    #
-    #     data.append({
-    #         'name': achievement.name,
-    #         'description': achievement.description,
-    #         'game': {
-    #             'id': game.id,
-    #             'name': game.name,
-    #             'description': game.description
-    #         }
-    #     })
-    #
-    # # Convert the data to JSON
-    # response = jsonify(data)
-    #
-    # # Return the response
-    # return response
