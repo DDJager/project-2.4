c20aa52481812f5ff451fb83eedb59030b0e6e2e
jlsa <jlsahoekstra@gmail.com>
2017-07-03 22:21:12 +0200

    Additional changes and updates to the front-end code

--- website/main/front-end/src/components/match_history.js
+++ website/main/front-end/src/components/match_history.js
@@ -2,22 +2,31 @@ import React from 'react';
 
 const MatchHistory = (props)=>{
     const {stats} = props;
-    if (!props.stats) {return <h3>loading</h3>}
-   return (
-       <div className="content-section z-depth-2 grey lighten-5">
-           <div className="row">
-               <div className="col s10 offset-s1">
-                    <div className="content-text-section">
-                       <h4><b>Match History</b></h4><br/>
-                       <div>
-                          games won: {stats.won}<br/>
-                          games lost: {stats.lost}<br/>
-                          games played: {stats.played}
-                       </div>
-                    </div>
-                </div>
+    if (!props.stats) {
+      return (
+          <div className="card">
+              <div className="content-text-section">
+                  <h4>
+                    Loading...
+                  </h4>
+              </div>
+          </div>
+      )
+    }
+    return (
+        <div className="col s5 offset-s1">
+            <div className="content-text-section">
+              <h4>
+                  Match History
+              </h4>
+              <br/>
+              <div>
+                  Games won: {stats.won}<br/>
+                  Games lost: {stats.lost}<br/>
+                  Games played: {stats.played}
+              </div>
             </div>
-       </div>
+        </div>
    )
 };
 
--- website/main/front-end/src/components/profile_user.js
+++ website/main/front-end/src/components/profile_user.js
@@ -6,9 +6,26 @@ import React, { Component } from 'react';
 class Id extends Component {
     render() {
         if (!this.props.user) {
-            return (<h4>user not found</h4>);
+          return (
+              <div className="content-section z-depth-2 grey lighten-5">
+                  <div className="row">
+                      <div className="col s10 offset-s1">
+                          <div className="content-text-section">
+                              <div className="profile-picture">
+                                  <img src="https://api.adorable.io/avatars/100/unknown.png" alt="no-user-found-img" width={100} height={100} />
+                              </div>
+                              <div className="profile-info">
+                                  <h3>@....</h3>
+                                  <p style={{'whiteSpace': 'pre-line'}}>There is no description as there is no user.</p>
+                              </div>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          )
         }
-        return(
+        return (
             <div className="content-section z-depth-2 grey lighten-5">
                 <div className="row">
                     <div className="col s10 offset-s1">
