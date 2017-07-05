e6e42bffbacb617f06fc54bca40fe09eda772d81
jlsa <jlsahoekstra@gmail.com>
2017-07-05 08:19:45 +0200

    added edit button to profile if you are viewing your own profile

--- website/main/front-end/src/components/profile_user.js
+++ website/main/front-end/src/components/profile_user.js
@@ -1,9 +1,16 @@
 //this component shows the name and profile picture
 //if you can think of a better file name change it
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Id extends Component {
+
+    editButton(user) {
+        if (user.id == localStorage.getItem('id')) {
+            return <Link to="/edit" className="btn">Edit</Link>
+        }
+    }
     render() {
         if (!this.props.user) {
           return (
@@ -24,25 +31,28 @@ class Id extends Component {
                   </div>
               </div>
           )
-        }
-        return (
-            <div className="content-section z-depth-2 grey lighten-5">
-                <div className="row">
-                    <div className="col s10 offset-s1">
-                        <div className="content-text-section">
-                            <div className="profile-picture">
-                                <img src={this.props.user.picture_url} alt={this.props.user.username} width={100} height={100} />
-                            </div>
-                            <div className="profile-info">
-                                <h3>@{this.props.user.username}</h3>
-                                <p style={{'whiteSpace': 'pre-line'}}>{this.props.user.description}</p>
-                            </div>
+        } else {
+          return (
+              <div className="content-section z-depth-2 grey lighten-5">
+                  <div className="row">
+                      <div className="col s10 offset-s1">
+                          <div className="content-text-section">
+                              <div className="profile-picture">
+                                  <img src={this.props.user.picture_url} alt={this.props.user.username} width={100} height={100} />
+                              </div>
+                              <div className="profile-info">
+                                  <h3>@{this.props.user.username}</h3>
+                                  <p style={{'whiteSpace': 'pre-line'}}>{this.props.user.description}</p>
+                                  {this.editButton(this.props.user)}
+                              </div>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          )
+        }
+
     }
 }
 
